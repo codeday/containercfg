cf9757cadfa1d05ad866776207ec4365de549b85
Tyler Menezes <tylermenezes@gmail.com>
2020-03-30 18:48:47 -0400

    Add IMAGE_TAG env variable, update canaries on John

--- jobs/lib/merge-previous.js
+++ jobs/lib/merge-previous.js
@@ -1,42 +1,42 @@
-const splitImage = (image) => {
-  const lastIndex = image.lastIndexOf(':');
-  return [ image.substr(0, lastIndex), image.substr(lastIndex + 1)];
-}
+const splitImage = image => {
+  const lastIndex = image.lastIndexOf(":");
+  const repo = image.substr(0, lastIndex);
+  const tag = image.substr(lastIndex + 1) || "latest";
+  return [repo, tag];
+};
 
-const parsePreviousJob = (jobInfo) => {
+const parsePreviousJob = jobInfo => {
   // Convert each group to K=>V object (instead of an array) and provide count + task info.
   const groupsOut = {};
-  jobInfo.TaskGroups.forEach((group) => {
-
+  jobInfo.TaskGroups.forEach(group => {
     // Also convert each task to K=>V and provide repo + image for Docker containers.
     const tasksOut = {};
-    group.Tasks.forEach((task) => {
-
+    group.Tasks.forEach(task => {
       // Only Docker containers have an image. All other tasks won't return any info.
-      if (task.Driver === 'docker') {
-        const [ repo, tag ] = splitImage(task.Config.image);
+      if (task.Driver === "docker") {
+        const [repo, tag] = splitImage(task.Config.image);
         tasksOut[task.Name] = {
           repo,
-          tag,
+          tag
         };
       }
     });
 
     groupsOut[group.Name] = {
       count: group.Count,
-      tasks: tasksOut,
+      tasks: tasksOut
     };
   });
 
   return groupsOut;
-}
+};
 
 module.exports = (job, previousJob) => {
   if (!job.TaskGroups || !previousJob.TaskGroups) return job;
   const defaultVars = parsePreviousJob(previousJob);
 
   // Overwrite count for each group which already has a running count.
-  job.TaskGroups = job.TaskGroups.map((group) => {
+  job.TaskGroups = job.TaskGroups.map(group => {
     if (!(group.Name in defaultVars)) return group;
     const defaultVarsGroup = defaultVars[group.Name];
 
@@ -45,16 +45,23 @@ module.exports = (job, previousJob) => {
     if (!group.Tasks) return group;
 
     // Overwrite image tag for each Docker task with the same specified repo.
-    group.Tasks = group.Tasks.map((task) => {
+    group.Tasks = group.Tasks.map(task => {
       if (!(task.Name in defaultVarsGroup.tasks)) return task;
       const defaultVarsTask = defaultVarsGroup.tasks[task.Name];
 
-      if (task.Driver === 'docker') {
-        const [ repo, tag ] = splitImage(task.Config.image);
-        if (defaultVarsTask.repo && defaultVarsTask.tag && defaultVarsTask.repo === repo) {
+      if (task.Driver === "docker") {
+        const [repo, tag] = splitImage(task.Config.image);
+        if (
+          defaultVarsTask.repo &&
+          defaultVarsTask.tag &&
+          defaultVarsTask.repo === repo
+        ) {
           task.Config.image = `${repo}:${defaultVarsTask.tag}`;
         }
       }
+
+      if (task.Env == null) task.Env = {};
+      task.Env.IMAGE_TAG = splitImage(task.Config.image)[1];
 
       return task;
     });
@@ -63,4 +70,4 @@ module.exports = (job, previousJob) => {
   });
 
   return job;
-}
+};
