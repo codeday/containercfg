30b491b053644b459118b158d0ed029c0bbe0d31
Tyler Menezes <github@xanadu.ws>
2020-07-09 09:22:09 -0400

    Add datadog

--- agents/puppet/modules/site/manifests/std.pp
+++ agents/puppet/modules/site/manifests/std.pp
@@ -1,5 +1,7 @@
 class site::std {
-  exec { "/usr/bin/apt-get update": }
+  exec { "apt-get update":
+    command => "/usr/bin/apt-get update",
+  }
 
   class { "sudo": }
   sudo::conf { 'sudo':
@@ -19,5 +21,28 @@ class site::std {
 
   package { ["zip", "unzip"]:
     ensure => latest,
+  }
+
+  package { "apt-transport-https":
+    ensure => latest,
+  }
+
+  $dd_key = "A2923DFF56EDA6E76E55E492D3A80E30382E94DE"
+  exec { "apt-key datadog":
+    command => "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key ${dd_key}",
+    unless  => "apt-key list | grep '${dd_key}' | grep -v expired",
+  }
+
+  file { "apt-repo datadog":
+    path    => "/etc/apt/sources.list.d/datadog.list",
+    ensure  => "file",
+    content => "deb https://apt.datadoghq.com/ stable 7",
+    notify  => Exec["apt-get update"],
+    require => Exec["apt-key datadog"],
+  }
+
+  package { "datadog-agent":
+    ensure => latest,
+    require => File["apt-repo datadog"],
   }
 }
