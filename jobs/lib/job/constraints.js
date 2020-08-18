1df41902854f8578e11d9884952769c6c0b38120
Tyler Menezes <github@xanadu.ws>
2020-08-18 08:41:14 -0400

    Move non-running jobs to different folder

--- jobs/lib/job/constraints.js
+++ jobs/lib/job/constraints.js
@@ -1,39 +1,38 @@
 const getPlacementConstraints = (placement) => {
-  if (!placement) return [];
   const constraints = [];
 
-  if (placement.type === 'unique') constraints.push({
+  if (placement && placement.type === 'unique') constraints.push({
     Operand: 'distinct_hosts',
   });
 
-  if (placement.os_type) constraints.push({
+  if (placement && placement.os_type) constraints.push({
     LTarget: '${attr.kernel.name}',
     Operand: '==',
     RTarget: placement.os_type,
   });
 
-  if (placement.os) constraints.push({
+  if (placement && placement.os) constraints.push({
     LTarget: '${attr.os.name}',
     Operand: '==',
     RTarget: placement.os,
   });
 
-  if (placement.os_version) constraints.push({
+  if (placement && placement.os_version) constraints.push({
     LTarget: '${attr.os.version}',
     Operand: '>=',
     RTarget: placement.os_version,
   });
 
-  if (placement.os) constraints.push({
+  if (placement && placement.os) constraints.push({
     LTarget: '${meta.host}',
     Operand: '==',
     RTarget: placement.host_dc,
   });
 
-  constraints.push({
+  if (!placement || (placement && placement.speed !== null)) constraints.push({
     LTarget: '${meta.speed}',
     Operand: '==',
-    RTarget: placement.speed || 'gbps',
+    RTarget: placement && placement.speed || 'gbps',
   });
 
 
